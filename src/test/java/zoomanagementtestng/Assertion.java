ca7de7e675a10fe75374fefd8071237beed1c0e9
Admin <Admin@DESKTOP-C0O15PC>
2022-09-21 13:09:29 +0530

    updated by engg1 assertion

--- src/test/java/zoomanagementtestng/Assertion.java
+++ src/test/java/zoomanagementtestng/Assertion.java
@@ -2,11 +2,16 @@ package zoomanagementtestng;
 
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
+
 public class Assertion {
 
 	@Test
-	//gdg1
+	
 		void test() {
+	System.out.println("the");
+		System.out.println("hey");
+		System.out.println("tqq");
 		System.out.println("hello");
 		System.out.println("hi");
 	}
